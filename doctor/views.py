d6f7b8592be4295af7123040dc82478d6494ec14
Tarun Batra <tarun.batra11@gmail.com>
2015-12-30 15:47:08 +0530

    Added feedback approval and deny option by admin, also added admin panel button for admin

--- doctor/views.py
+++ doctor/views.py
@@ -11,27 +11,32 @@ logger = logging.getLogger(__name__)
 
 @login_required
 def index(request):
+    user = request.user
+    if user.is_superuser:
+        feedbacks = Feedback.objects.filter(approved_status='pending')
+        return render(request, 'index.html', {'feedbacks': feedbacks})
+
     return render(request, 'index.html')
 
 
 @login_required
 def doctor(request):
     user = str(request.user)
-    user_object = User.objects.get(username = user)
-    account_object = Account.objects.get(user = user_object)
-    doctor_object = Doctor.objects.get(name = account_object)
+    user_object = User.objects.get(username=user)
+    account_object = Account.objects.get(user=user_object)
+    doctor_object = Doctor.objects.get(name=account_object)
     fo = str(timezone.now())[:19]
     new_requests = Appointment.objects.filter(doctor=doctor_object, approved_status='pending', appointment_time__gte=fo)
     approved_appointments = Appointment.objects.filter(doctor=doctor_object, approved_status='approved', appointment_time__gte=fo)
-    feedbacks_received = Feedback.objects.filter(doctor=doctor_object)
+    feedbacks_received = Feedback.objects.filter(doctor=doctor_object, approved_status='approved')
     return render(request, 'doctor.html', {'new_requests' : new_requests, 'approved_appointments': approved_appointments, 'feedbacks_received': feedbacks_received})
 
 
 @login_required
 def patient(request):
     user = str(request.user)
-    user_object = User.objects.get(username = user)
-    account_object = Account.objects.get(user = user_object)
+    user_object = User.objects.get(username=user)
+    account_object = Account.objects.get(user=user_object)
     appointment_history = Appointment.objects.filter(patient=account_object)
     approved_appointments = Appointment.objects.filter(patient=account_object, approved_status='approved')
     fo = str(timezone.now())[:19]
@@ -60,6 +65,19 @@ def appointment_approve(request, pk):
 @login_required
 def appointment_reject(request, pk):
     appointment = get_object_or_404(Appointment, pk=pk)
-    #post_pk = comment.post.pk
     appointment.reject()
     return redirect('doctor.views.doctor')
+
+
+@login_required
+def feedback_approve(request, pk):
+    feedback = get_object_or_404(Feedback, pk=pk)
+    feedback.approve()
+    return redirect('doctor.views.index')
+
+
+@login_required
+def feedback_reject(request, pk):
+    feedback = get_object_or_404(Feedback, pk=pk)
+    feedback.reject()
+    return redirect('doctor.views.index')
