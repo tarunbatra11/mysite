8598b942d966aaecf9d54f2afe44c418333ae776
Tarun Batra <tarun@atlogys.com>
2017-09-14 11:40:48 +0530

    add files for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='mysite',
+    packages=['mysite'],
+    version='0.1',
+    description='Managing doctor and patients',
+    author='Tarun Batra',
+    author_email='tarun.batra11@gmail.com',
+    url='https://github.com/tarunbatra11/mysite',
+    download_url='https://github.com/tarunbatra11/mysite/archive/v1.tar.gz',
+    keywords=['doctor', 'patient', 'meeting'],
+    classifiers=[],
+)
